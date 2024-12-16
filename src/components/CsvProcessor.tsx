4e75931fd6e081ec58c98cbd4ed9d55c46aece23
Danish Shakeel <danishshakeel54@gmail.com>
2024-12-16 15:41:30 +0100

    fix: ts issues

--- src/components/CsvProcessor.tsx
+++ src/components/CsvProcessor.tsx
@@ -26,7 +26,7 @@ const fadeInUp = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
   exit: { opacity: 0, y: -20 },
-  transition: { duration: 0.2 },
+  transition: { duration: 0.3 },
 };
 
 export default function CsvProcessor() {
